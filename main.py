2133e4f3484158b3a7bf94649c99323fcf56398a
Jacqueline Roberti <jacqueline.roberti93@gmail.com>
2021-07-12 16:09:56 -0400

    pagerduty page function

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ import os
 
 import flask
 from slack.signature import SignatureVerifier
+import pdpyras
 
 # SLACK_SIGNING_SECRET: secret/suitable/terraform/pagerduty-trigger-slack-command/slack-token @ slack-signing-secret
 # We might not need the client secret?
 # SLACK_CLIENT_SECRET: secret/suitable/terraform/pagerduty-trigger-slack-command/slack-token @ slack-client-secret
 # PAGERDUTY_INTEGRATION_SECRET: secret/suitable/pagerduty/manually-triggered-terra-incident @ events-v2-integration-key
-
+PAGERDUTY_SOURCE = 'app.terra.bio'
 
 def verify_signature(request: flask.Request) -> None:
     request.get_data()  # Decodes received requests into request.data
     verifier = SignatureVerifier(os.environ['SLACK_SIGNING_SECRET'])
     if not verifier.is_valid_request(request.data, request.headers):
         raise ValueError('Invalid request/credentials.')
+
+
+def trigger_pagerduty(message, source):
+    session = pdpyras.EventsAPISession(os.environ('PAGERDUTY_INTEGRATION_SECRET'))
+    session.trigger(message, source)
+    return "Page initiated." 
 
 
 def terra_is_broken(request: flask.Request):
@@ -34,4 +41,5 @@ def terra_is_broken(request: flask.Request):
 
     # Like /terraisbroken <argument>
     command_argument = request.form['text']
-    return flask.escape("something")
+    page = trigger_pagerduty(command_argument, PAGERDUTY_SOURCE)
+    return flask.escape(page)
